827643901198e11a537ea8952359ee0a0ae68d46
anderson <andersonpayao018@gmail.com>
2024-02-29 22:30:28 -0300

    json

---
+++ json/localidades/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", async function(){
+    const listamunicipios = document.getElementById("municipios-list");
+
+    const resposta = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome')
+
+    const dados = await resposta.json();
+
+    dados.forEach(function (municipio) {
+        const li = document.createElement('li');
+
+        li.textContent = municipio.nome;
+
+        listamunicipios.appendChild(li);
+        
+    });
+}
+
+
+)
