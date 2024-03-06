e0a636acf7e80f057c8f370ca598dd7759edf9c7
anderson <andersonpayao018@gmail.com>
2024-03-05 22:20:48 -0300

    sistema de inflação

--- json/inflação/script.js
+++ json/inflação/script.js
@@ -1,13 +1,46 @@
-document.addEventListener("DOMContentLoaded", async function(){  
-    const container = document.getElementById("container");
+document.addEventListener("DOMContentLoaded", async function () {
+  const conteudo = document.getElementById("conteudo");
 
-    const resposta = await fetch('https://servicodados.ibge.gov.br/api/v3/agregados/1705/variaveis?view=OLAP&localidades=BR');
+  const resposta = await fetch(
+    "https://servicodados.ibge.gov.br/api/v3/agregados/1705/variaveis?view=OLAP&localidades=BR"
+  );
 
-    const dados = await resposta.json();    
+  const dados = await resposta.json();
 
-    dados.forEach( function(cnae) {       
+  dados.forEach(function (inflacao) {
+    const divBlocoInflacao = document.createElement("div");
+    divBlocoInflacao.classList.add("bloco-inflacao");
 
+    const ulSubtitulo = document.createElement("ul");
+    ulSubtitulo.id = "subtitulo";
+
+    const liSubtitulo = document.createElement("li");
+    liSubtitulo.textContent = `${inflacao.medida} - (${inflacao.unidade})`;
+
+    ulSubtitulo.appendChild(liSubtitulo);
+
+    divBlocoInflacao.appendChild(ulSubtitulo);
+
+    const resultados = inflacao.resultados;
+    resultados.forEach(function (resultado) {
+      const olResultado = document.createElement("ol");
+
+      const series = resultado.series;
+
+      series.forEach(function (seriegeral) {
+        const serieData = seriegeral.serie;
+
+        for (const anoMes in serieData) {
+          const liSerie = document.createElement("li");
+          liSerie.textContent = `${anoMes} - ${serieData[anoMes]}`;
+
+          olResultado.appendChild(liSerie);
+        }
+      });
+
+      ulSubtitulo.appendChild(olResultado);
     });
-}
-)  
-  
+
+    conteudo.appendChild(divBlocoInflacao);
+  });
+});
